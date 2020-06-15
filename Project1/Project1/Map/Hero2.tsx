b8cc49d6c24600dd8ccacefbe62e453fcf6d49c3
Veonika <63860942+Veonika@users.noreply.github.com>
2020-06-15 13:46:15 +0300

    Delete Hero2.tsx

--- Project1/Project1/Map/Hero2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.5" name="Hero2" tilewidth="32" tileheight="32" spacing="2" tilecount="238" columns="17">
- <image source="наша акула1.png" width="600" height="495"/>
-</tileset>
