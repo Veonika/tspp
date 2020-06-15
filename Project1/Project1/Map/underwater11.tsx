ab1d2b24c48ba1dc1a842838eb0689120dde2f4d
Veonika <63860942+Veonika@users.noreply.github.com>
2020-06-15 13:44:11 +0300

    Delete underwater11.tsx

--- Project1/Project1/Map/underwater11.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.4" name="underwater11" tilewidth="32" tileheight="32" tilecount="3072" columns="64">
- <image source="underwater.png" trans="616f58" width="2050" height="1540"/>
-</tileset>
