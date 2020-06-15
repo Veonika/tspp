9a260c93964b4cc26ddc81658a7c209f8d664f73
Veonika <63860942+Veonika@users.noreply.github.com>
2020-06-15 13:46:22 +0300

    Delete Hero1.tsx

--- Project1/Project1/Map/Hero1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.5" name="Simple_Fish" tilewidth="32" tileheight="32" spacing="2" tilecount="272" columns="16">
- <image source="смешная рыбка.png" width="551" height="593"/>
-</tileset>
