b286253c15cf766964d267fdff3ec22d95b153d3
Veonika <63860942+Veonika@users.noreply.github.com>
2020-06-15 13:44:41 +0300

    Delete Colour_Fish.tsx

--- Project1/Project1/Map/Colour_Fish.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.5" name="Colour_Fish" tilewidth="32" tileheight="32" tilecount="4278" columns="93">
- <image source="сиреневая рыбка.png" width="3000" height="1500"/>
-</tileset>
